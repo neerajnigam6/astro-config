9fc72fe0a5db33e5167a38bd2d996ad5759be3e3
neeraj <neerajnigam6@gmail.com>
2023-07-06 14:52:24 +0530

    fit: updated with dart utils and flutter tools

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,10 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin",   enabled = true },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+  { import = "astrocommunity.pack.dart" }
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
